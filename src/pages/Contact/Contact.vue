398541ec34d653b8224b03664ad0bd45948988f2
曾海艳 <Annyhaiyan@lantis.local>
2018-06-10 16:02:20 +0800

    modify contact.vue

--- src/pages/Contact/Contact.vue
+++ src/pages/Contact/Contact.vue
@@ -2,7 +2,7 @@
   <nas-page-content class="nas-page-content-contact">
     <p>如果您在使用过程中遇到任何问题，请与我联系</p>
     <p>Email：<a href="mailto:463668959@qq.com">463668959@qq.com</a></p>
-    <p>Github：<a href="https://github.com/lantislin" target="_blank">https://github.com/lantislin</a></p>
+    <p>Github：<a href="https://github.com/change888" target="_blank">https://github.com/change888</a></p>
   </nas-page-content>
 </template>
 
